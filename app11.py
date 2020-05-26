bac9742ffb8acb8fe229a068aff1df2e533f40a9
jourgui <jourdain.guillaume.pro@gmail.com>
2020-05-27 02:38:15 +0900

    update message

--- app11.py
+++ app11.py
@@ -427,36 +427,14 @@ def set_display_children(client_sk_num):
     elif b4 == 0:
         age_employment_experience_critical = 'low'
 
-#Age, employment experience, registration and publication date rating
-    return (u'The client number {} with a score of {}/95 has been {} to loan with a probability of {:.3f} and \
-        threshold of {} based on the following criteria in order of significance. \
-        in order of significance. The client has {} total years of loan (critical value: {}). The client has \
-        ${} of prescribed installment amount of previous credit on this installment (critical value: ${}), \
-                 {:.2f} as residency external option rating (critical value: {:.2f}), and {}  paid \
-                     installments (critical value: {}). The client had {} drawings for a month, 60 days ago (critical value: {})\
-                         and {} days past due during the month of previous credit (critical value: {}). The \
-                              client region with city rating is {} (critical value: {}). The education income \
-                                   rating is {} (critical value: {}). The client age, employment experience, registration and publication date rating is {} (critical value: {}). This is a {} (critical value: {}).'.format(client_sk_num,int(score_client),target_message,target,Threshold_loan,
-    int(df['Total years of loan (loan/annuity)'][df['SK_ID_CURR'] == client_sk_num].iloc[0]),
-    int(df_xgb_limit['Total years of loan (loan/annuity)'].values[0]),
-    int(df['Prescribed installment amount of previous credit on this installment'][df['SK_ID_CURR'] == client_sk_num].iloc[0]),
-    int(df_xgb_limit['Prescribed installment amount of previous credit on this installment'].values[0]),
-    df['Residency external option rating'][df['SK_ID_CURR'] == client_sk_num].iloc[0],
-    df_xgb_limit['Residency external option rating'].values[0],
-    int(df['Number of paid installment'][df['SK_ID_CURR'] == client_sk_num].iloc[0]),
-    int(df_xgb_limit['Number of paid installment'].values[0]),
-    int(df['Number of drawings for a month, 60 days ago'][df['SK_ID_CURR'] == client_sk_num].iloc[0]),
-    int(df_xgb_limit['Number of drawings for a month, 60 days ago'].values[0]),
-    int(df['Days past due during the month of previous credit'][df['SK_ID_CURR'] == client_sk_num].iloc[0]),
-    int(df_xgb_limit['Days past due during the month of previous credit'].values[0]),
-    city_rating,
-    city_rating_critical,
-    education_income_rating,
-    education_income_rating_critical,
-    age_employment_experience, 
-    age_employment_experience_critical,
-    cash_revolving_loan,
-    cash_revolving_loan_critical))
+    return (u'The client number {} has been {} to loan with a probability of {:.3f} and \
+        threshold of {} based on the following criteria in order of significance: \
+         the total years of loan, the prescribed installment amount of previous credit on this installment (in $), \
+                 the residency external option rating, the paid installments, the number drawings for a month, 60 days ago, \
+                           the number of days past due during the month of previous credit, the client region with city rating, \
+                              the education income rating, the client age, employment experience, registration and publication date rating and \
+                                   the cash or revolving loan status.'.format(client_sk_num,target_message,target,Threshold_loan)
+    
 
 @dash_app1.callback(
     Output('table','data'),
